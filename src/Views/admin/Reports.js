c27c5e9e975170f771acd12939d3f21c0a42e1bd
NicoAroyo <nico.aroyo@gmail.com>
2022-08-25 15:19:48 +0300

    caused my depression

--- src/Views/admin/Reports.js
+++ src/Views/admin/Reports.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 export const Reports = () => {
+  const navigate = useNavigate();
   return <div>Reports</div>;
+  <button onClick={()=>navigate(-1)}>Back</button>
 };
